c2c520a1df78cb10b1a6a7f9996d1d3cfb40b356
VishalBhuse <vbhuse1997@gmail.com>
2023-06-30 15:20:49 +0530

    flow complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import GeneratePassword from "./GeneratePassword";
+const App = () => {
+  const [password, setPassword] = useState("");
+  const [length, setLength] = useState(6);
+  const [uppercase, SetUppercase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [symbols, setSymbols] = useState(false);
 
-function App() {
+  const handleRange = (e) => {
+    setLength(Number(e.target.value));
+  };
+
+  const handleGeneratePass = () => {
+    const generatedPassword = GeneratePassword(
+      length,
+      uppercase,
+      numbers,
+      symbols
+    );
+    setPassword(generatedPassword);
+    navigator.clipboard.writeText(generatedPassword);
+  };
+
+  const handleFilter = (e) => {
+    const { name, checked } = e.target;
+
+    if (name === "uppercase") {
+      SetUppercase(checked);
+    } else if (name === "numbers") {
+      setNumbers(checked);
+    } else if (name === "symbols") {
+      setSymbols(checked);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main_container">
+      <div className="parent_div">
+        <h3>Password Generator</h3>
+        <div className="pass_copy">
+          <p>{password}</p>
+          <i
+            className="fas fa-copy cpysvg"
+            onClick={() => navigator.clipboard.writeText(password)}
+          ></i>
+        </div>
+        <div className="secodnchild">
+          <div className="char_len">
+            <p>charater length</p>
+            <p>{length}</p>
+          </div>
+          <div className="range">
+            <input
+              type="range"
+              min="6"
+              max="15"
+              className="rangeinput"
+              value={length}
+              onChange={handleRange}
+            />
+          </div>
+          <div className="filter">
+            <div>
+              <input
+                type="checkbox"
+                name="uppercase"
+                checked={uppercase}
+                onChange={handleFilter}
+              />
+              <span>include uppercase letter</span>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                name="numbers"
+                checked={numbers}
+                onChange={handleFilter}
+              />
+              <span>include number</span>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                name="symbols"
+                checked={symbols}
+                onChange={handleFilter}
+              />
+              <span>include symbols</span>
+            </div>
+          </div>
+          <button className="generate" onClick={handleGeneratePass}>
+            Generate
+            <i className="fas fa-arrow-right"></i>
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
